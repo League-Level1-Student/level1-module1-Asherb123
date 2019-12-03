ec5e8006feb622a3d19caf713c59dcb9a96e467a
League Student <league@iMac-34.attlocal.net>
2019-12-02 18:28:30 -0800

    I started level 0+1 mod 1

--- src/_01_race_car/RaceCarRunner.java
+++ src/_01_race_car/RaceCarRunner.java
@@ -11,14 +11,18 @@ public class RaceCarRunner {
 		/* Do the following things without changing the RaceCar class */
 		
 		// 1. Create a RaceCar and place it in 5th position
-	
+		RaceCar epicMcRacer=new RaceCar("Epicness McPerson", 5);
 		// 2. Print the RaceCar's position in the race
-
+		System.out.println(epicMcRacer.getPositionInRace());
 		// 3. Crash the RaceCar
-		
+		epicMcRacer.crash();
 		// 4. If the car is damaged. Bring it in for a pit stop.
-
+		epicMcRacer.pit();
 		// 5. Help the car move into first place.
-
-	}
+		
+		for (int i = 0; i < 7; i++) {
+			
+		
+		epicMcRacer.overtake();
+	}}
 }

4d5e8265bf474478a7e3c9cfd1d03ef28e18bb02
League Student <league@iMac.attlocal.net>
2019-12-09 18:27:42 -0800

    YEEEEEEEE

--- src/_02_cat/CatRunner.java
+++ src/_02_cat/CatRunner.java
@@ -1,5 +1,45 @@
 package _02_cat;
 
 public class CatRunner {
+public static void main(String[] args) {
+	
+	
+Cat catsworth = new Cat("doichlad");
+for (int i = 0; i < 9; i++) {
+	
 
+catsworth.printName();
+
+catsworth.kill();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 }
---
+++ src/_03_harry_potter/HarryPotterRunner.java
@@ -0,0 +1,44 @@
+package _03_harry_potter;
+
+public class HarryPotterRunner {
+public static void main(String[] args) {
+	
+
+HarryPotter potty = new HarryPotter();
+
+
+potty.makeInvisible(true);
+
+
+potty.spyOnSnape();
+
+potty.makeInvisible(true);
+
+potty.castSpell("doich");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}

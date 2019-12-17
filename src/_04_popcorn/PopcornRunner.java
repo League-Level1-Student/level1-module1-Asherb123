67285b501448c43b40d5ad400783ffbd55200db8
League Student <league@iMac.attlocal.net>
2019-12-16 18:28:54 -0800

    yee

--- src/_04_popcorn/PopcornRunner.java
+++ src/_04_popcorn/PopcornRunner.java
@@ -1,5 +1,34 @@
 package _04_popcorn;
 
 public class PopcornRunner {
+public static void main(String[] args) {
+	
+Popcorn littleBoi = new Popcorn("yeet");	
+Microwave daddy = new Microwave();
 
+
+daddy.putInMicrowave(littleBoi);
+daddy.setTime(5);	
+daddy.startMicrowave();	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
 }
---
+++ src/_05_vault/Vault.java
@@ -0,0 +1,12 @@
+package _05_vault;
+
+public class Vault {
+
+int SecretCode;	
+boolean tryCode(int SecretCode) {
+	if (this.SecretCode==SecretCode) {
+		return false; 
+		}
+}
+}
+
